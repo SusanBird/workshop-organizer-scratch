81829b5624422dfef0b767ecce683dccb06abf65
SusanBird <susanbird391@gmail.com>
2022-04-01 12:34:53 -0700

    get data from tables

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,7 +1,48 @@
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_URL = 'https://iqcxlznkhjgmeesvmysg.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxY3hsem5raGpnbWVlc3ZteXNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg4MzQzMjYsImV4cCI6MTk2NDQxMDMyNn0.LZ8bVoiP1UVVS42XlcfqiKDPYaQmPpIdGECR9vZpevM';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getWorkshops() {
+    const response = await client
+        .from('workshops')
+        .select('*, participants (*)');
+
+    return response.body;
+}
+
+export async function getParticipant(someId) {
+    const response = await client
+        .from('participants')
+        .select('*')
+        .match({ id: someId })
+        .single();
+
+    return response.body;
+}
+
+export async function createParticipant(name, workshop_id) {
+    const response = await client
+        .from('participants')
+        .insert({
+            name: name,
+            workshop_id: workshop_id
+        });
+
+    return response.body;
+}
+
+export async function editParticipant(id, name, workshop_id) {
+    const response = await client
+        .from('participants')
+        .update({
+            name: name,
+            workshop_id: workshop_id
+        })
+        .match({ id: id });
+
+    return response.body;
+}
 
 export function getUser() {
     return client.auth.session() && client.auth.session().user;
