1ec7059925a77980e39be8605d22030afe1bc937
SusanBird <susanbird391@gmail.com>
2022-04-02 17:58:23 -0700

    fixed edit-part dropdown

--- edit-participant/edit-participant.js
+++ edit-participant/edit-participant.js
@@ -1,9 +1,24 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, logout, getWorkshops } from '../fetch-utils.js';
 
 checkAuth();
 
 const logoutButton = document.getElementById('logout');
+const selectEl = document.querySelector('select');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+
+window.addEventListener('load', async () => {
+    const workshops = await getWorkshops();
+
+    for (let workshop of workshops) {
+        const optionEl = document.createElement('option');
+
+        optionEl.textContent = workshop.topic;
+        optionEl.value = workshop.id;
+
+        selectEl.append(optionEl);
+    }
 });
